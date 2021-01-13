870342539d825ce2886cbdf6420ce17a8dbc1e84
Syed Towhid <towhid@hotmail.com>
2021-01-13 19:41:26 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,6 @@
 module "s3-bucket" {
-  source  = "terraform-aws-modules/s3-bucket/aws"
+  source  = "app.terraform.io/SyedT-Training/s3-bucket/aws"
   version = "1.17.0"
-  # insert the 5 required variables here
+  # insert required variables here
   bucket_prefix = "${var.prefix}"
 }

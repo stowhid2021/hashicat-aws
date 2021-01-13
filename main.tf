08982c06e0cd86df5c38a22a57c14590c072af58
Syed Towhid <towhid@hotmail.com>
2021-01-13 19:46:40 +0000

    Added s3 bucket module

--- main.tf
+++ main.tf
@@ -125,8 +125,8 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-    department = "devops"
-    billable = "true"
+    Department = "devops"
+    Billable = "true"
   }
 }
 
